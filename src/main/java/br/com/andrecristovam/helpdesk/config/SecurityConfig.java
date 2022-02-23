5806995d8c20e4b8f3c20173f2b48245e6a25c76
andre cristovam <andrecristovam@gmail.com>
2022-02-22 23:33:10 -0300

    Correção Cors setMethods

--- src/main/java/br/com/andrecristovam/helpdesk/config/SecurityConfig.java
+++ src/main/java/br/com/andrecristovam/helpdesk/config/SecurityConfig.java
@@ -25,7 +25,7 @@ import br.com.andrecristovam.helpdesk.security.JWTUtil;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
-	private static final String[] PUBLIC_MATCHER = {"/h2-console/**"};
+	private static final String[] PUBLIC_MATCHER = {"/h2-console/**", "*/**"};
 	
 	@Autowired
 	private Environment env;
@@ -36,35 +36,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsService userDetailsService;
 	
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		
-//		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
-//			http.headers().frameOptions().disable();
-//		}
-//		
-//		http.cors().and().csrf().disable();
-//		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-//		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
-//		http.authorizeRequests().antMatchers(PUBLIC_MATCHER).permitAll()
-//								.anyRequest().authenticated();		
-//		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//	}
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
 		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
 		
-		http.cors().and().csrf().disable();		
+		http.cors().and().csrf().disable();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));		
-		http.authorizeRequests()
-						.antMatchers(PUBLIC_MATCHER).permitAll() 
-						.anyRequest().authenticated(); 		
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);	
-	}
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+		http.authorizeRequests().antMatchers(PUBLIC_MATCHER).permitAll()
+								.anyRequest().authenticated();		
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}	
+
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -75,8 +61,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-		configuration.setAllowedHeaders(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS", "CONNECT", "TRACE", "PATCH", "HEAD"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		
+		configuration.setAllowCredentials(true);
+		configuration.addAllowedHeader("*");
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setMaxAge(3600L);
+		
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 		
